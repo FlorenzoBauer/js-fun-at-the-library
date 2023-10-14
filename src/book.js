f92f0314887e995d5edc599b13f66c1eb399615a
Florenzo Bauer <florenzobauer@gmail.com>
2023-10-14 17:13:45 -0600

    Doing the warm up and Tests First ten tests passed on the book.js file.

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+function createTitle(title) {
+  var titles = title;
+  return "The " + titles;
 }
+function buildMainCharacter(name, age, pronouns){
+  var ghoulCharacter = {
+    
+      name: name,
+      age: age,
+      pronouns: pronouns,
+    }
+  
+  var bakerCharacter = 
+    {
+     name: name,
+     age: age,
+     pronouns: pronouns,
+    }
+  return ghoulCharacter, bakerCharacter;
+}
+function saveReview(input, reviews) {
+  var same = false;
+
+for (var i = 0; i < reviews.length; i++){
+  if (reviews[i] === input) {
+    same = true;
+    break;
+  }
+
+}
+if (!same) {
+  reviews.push(input);
+}
+return reviews;
+}
+function calculatePageCount() {
+
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+   buildMainCharacter,
+   saveReview,
+   calculatePageCount,
   // writeBook,
   // editBook
 }
